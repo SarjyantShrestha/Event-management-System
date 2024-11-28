698e77acf7e645a3c3223aaad4af92e14acf7916
manishpyakurel <manishpyakurel67@gmail.com>
2024-11-28 20:26:23 +0545

    quick action button working

--- Frontend/src/constants/index.js
+++ Frontend/src/constants/index.js
@@ -4,7 +4,7 @@ const navLinks = [
   { id: 3, name: "Event Booking", path: "/event-booking", icon: "fas fa-ticket-alt" },
   { id: 4, name: "User Details", path: "/user-details", icon: "fas fa-user" },
   { id: 5, name: "Venue", path: "/Spaces", icon: "fas fa-location-dot" },
-  { id: 5, name: "Manage Events", path: "/manage-events", icon: "fas fa-calendar-check" },
+  { id: 6, name: "Manage Events", path: "/manage-events", icon: "fas fa-calendar-check" },
 ];
 
 export default navLinks;
