82b44fc6a5d8175805358aa8072f299db203cdd9
Sarjyant <sarjyant@gmail.com>
2024-11-29 13:35:39 +0545

    query added

--- Backend/src/model/userModel.js
+++ Backend/src/model/userModel.js
@@ -3,7 +3,10 @@ import { getUserById, getAllUsers } from "../query/userQueries.js";
 
 export const fetchUserById = async (userId) => {
   try {
-    const result = await pool.query(getUserById, [userId]);
+    const result = await pool.query(
+      "SELECT id, username, email, created_at FROM users WHERE id = $1",
+      [userId],
+    );
     return result.rows[0]; // Return the single user
   } catch (error) {
     throw error; // Propagate the error for handling in the controller
@@ -12,7 +15,9 @@ export const fetchUserById = async (userId) => {
 
 export const fetchAllUsers = async () => {
   try {
-    const result = await pool.query(getAllUsers);
+    const result = await pool.query(
+      "SELECT id, username, email, created_at FROM users",
+    );
     return result.rows; // Return all users
   } catch (error) {
     throw error;
