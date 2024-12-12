8e2eab80b8a4d9715acf53d4e8891215e45a24a3
Sarjyant <sarjyant@gmail.com>
2024-12-12 22:10:10 +0545

    dashboard fetches

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { getSingleUser, getAllUsers, getUserProfile, deleteUser } from "../controllers/userController";
+import { getSingleUser, getAllUsers, getUserProfile, deleteUser, totalUsers } from "../controllers/userController";
 import verifyRole from "../middleware/verifyRole";
 import { authenticateToken } from "../middleware/authToken";
 
 const router = express.Router();
 
 router.get("/", verifyRole("admin"), getAllUsers);
+router.get("/totalusers", verifyRole("admin"), totalUsers);
 
 router.get("/:id", verifyRole("admin"), getSingleUser);
 
